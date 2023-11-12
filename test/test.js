fdef0526a22aa9c2206af5e99e415d51f6c8951a
Niko Cremer <n.ko@posteo.de>
2023-11-12 15:08:49 +0100

    linking the global.css and the component css to the main css file

--- test/test.js
+++ test/test.js
@@ -6,17 +6,22 @@
 // counter++;
 // counterValue.innerHTML = counter;
 
-let operand1 = 0;
-operand1++;
-console.log(operand1);
+// let counter = 0;
+// counter++;
+// console.log(counter);
 
-// function changeBmrk() {
-//   var image = document.getElementById("check_bkmrk");
-//   if (image.src.match("img/bookunmrk.png")) {
-//     image.src = "img/bookmrk.png";
-//     console.log("first-if-bookunmrk");
-//   } else {
-//     image.src = "img/bookunmrk.png";
-//     console.log("else-bookunmrk");
-//   }
-// }
+let currentCounter = 2;
+
+function changeBmrk() {
+  var image = document.getElementById("check_bkmrk");
+  if (image.src.match("img/bookunmrk.png")) {
+    image.src = "img/bookmrk.png";
+    console.log("first-if-bookunmrk");
+    currentCounter++;
+    console.log(currentCounter);
+    return currentCounter;
+  } else {
+    image.src = "img/bookunmrk.png";
+    console.log("else-bookunmrk");
+  }
+}
