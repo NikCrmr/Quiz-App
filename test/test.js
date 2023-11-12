37f1625b53fdabeb82ee3c2ca45cb4e026ca7d37
Niko Cremer <n.ko@posteo.de>
2023-11-12 15:04:33 +0100

    uplead test folder with files - to test out the js counter for bookmarks and questions

---
+++ test/test.js
@@ -0,0 +1,22 @@
+// Counter ****
+
+// let counter = 0;
+
+// const counterValue = document.getElementById('counter-value');
+// counter++;
+// counterValue.innerHTML = counter;
+
+let operand1 = 0;
+operand1++;
+console.log(operand1);
+
+// function changeBmrk() {
+//   var image = document.getElementById("check_bkmrk");
+//   if (image.src.match("img/bookunmrk.png")) {
+//     image.src = "img/bookmrk.png";
+//     console.log("first-if-bookunmrk");
+//   } else {
+//     image.src = "img/bookunmrk.png";
+//     console.log("else-bookunmrk");
+//   }
+// }
