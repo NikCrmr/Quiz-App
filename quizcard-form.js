79b4d002295b8802b614e7854445ef3cc6ebf065
Niko Cremer <n.ko@posteo.de>
2023-11-27 11:07:06 +0100

    added Create Element form to my cards html

--- quizcard-form.js
+++ quizcard-form.js
@@ -17,5 +17,49 @@ quizForm.addEventListener("submit", (event) => {
   const formQuestion = event.target.question;
   const formAnswer = event.target.answer;
   const formTags = event.target.tags;
-  console.log(formQuestion);
+  //console.log(formQuestion);
+
+//The New-Kids-For-Building a Quizcard
+const main = document.querySelector('[data-js="main"]');
+// Sticking The Building Blocks Together!!! Let's Play!!!
+const quizCardContainer = document.createElement('article');
+main.append.quizCardContainer;
+quizCardContainer.classList.add('quizcard');
+quizCardContainer.setAttribute(aria-labelledby, Quizcard);
+//quizcard Bookmark
+const quizCardBookmark = document.createElement('img');
+quizCardContainer.append.quizCardBookmark;
+quizCardBookmark.classList.add('bookmark-icon');
+quizCardBookmark.setAttribute(src, img/bookmrk.png);
+quizCardBookmark.setAttribute(alt, checked bookmark);
+quizCardBookmark.setAttribute(class, bookmark-icon);
+quizCardBookmark.setAttribute(id, check--bookmark);
+quizCardBookmark.setAttribute(onclick, changeBookmark());
+//quizcard Question
+const quizCardQuestion = document.createElement('h2');
+quizCardContainer.append.quizCardQuestion;
+//quizcard Button
+const quizCardButton = document.createElement('button');
+quizCardContainer.append.quizCardButton;
+quizCardButton.setAttribute(onclick,show());
+quizCardButton.classList.add('answer-button');
+quizCardButton.setAttribute(data-js,answer-button)
+quizCardButton.setAttribute(aria-label,Show Answer)
+//quizcard Answer
+const quizCardTags = document.createElement('span');
+const quizCardAnswer = document.createElement('p');
+quizCardContainer.append.quizCardAnswer;
+quizCardAnswer.classList.add('showanswer');
+quizCardAnswer.setAttribute(id, showanswer);
+quizCardAnswer.setAttribute(data-js, answer-text);
+//Quizcard div for tags
+const quizCardTagsDiv = document.createElement('div');
+quizCardContainer.append.quizCardTagsDiv;
+//quizcard category-tags span-elements
+const quizCardCategorySpans = document.createElement('span');
+quizCardCategorySpans.classList.add('cetegory');
+
 });
+          
+          
+          
